98ea21546a70eec83c374cebf344c32a23fc974e
Гаврилов Александр <gavrilov-al@mail.ru>
2022-09-12 19:44:59 +0300

    2

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -8,7 +8,6 @@ router_v1 = DefaultRouter()
 router_v1.register('tags', TagsViewSet, basename='tags')
 router_v1.register('ingredients', IngredientsViewSet, basename='ingredients')
 router_v1.register('recipes', RecipeViewSet, basename='recipes')
-router_v1.register('recipes', RecipeViewSet, basename='recipes')
 
 
 urlpatterns = [
