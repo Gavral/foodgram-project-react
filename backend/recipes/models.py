623591c0843cf93fbd4b0621e9e30c969ed0fa88
Гаврилов Александр <gavrilov-al@mail.ru>
2022-09-18 20:17:59 +0300

    model ingredient

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -33,7 +33,7 @@ class Ingredient(models.Model):
     )
 
     class Meta:
-        ordering = ('-id',)
+        ordering = ('name',)
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
 
