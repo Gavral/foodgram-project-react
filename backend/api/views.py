b794ae1bfc3e084a493f2bd0c0fcbcfc6d061706
Гаврилов Александр <gavrilov-al@mail.ru>
2022-09-26 19:08:38 +0300

    1

--- backend/api/views.py
+++ backend/api/views.py
@@ -4,12 +4,11 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import status
 from rest_framework.generics import ListAPIView
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.viewsets import ModelViewSet
+from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
 
-from .mixins import ListRetriveViewSet
 from .filters import IngredientSearchFilter, RecipeFilter
 from .pagination import CustomPageNumberPagination
 from .permissions import IsAuthorOrReadOnly
@@ -57,13 +56,15 @@ class FollowListAPIView(ListAPIView):
         return self.get_paginated_response(serializer.data)
 
 
-class TagsViewSet(ListRetriveViewSet):
+class TagsViewSet(ReadOnlyModelViewSet):
     queryset = Tag.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = TagSerializer
 
 
-class IngredientsViewSet(ListRetriveViewSet):
+class IngredientsViewSet(ReadOnlyModelViewSet):
     queryset = Ingredient.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = IngredientSerializer
     filter_backends = [IngredientSearchFilter]
     search_fields = ('^name',)
